4d408866092b2c80bed0ba3405567d835793b2bf
Atte Liimatainen <atte.liimat@gmail.com>
2021-11-15 12:15:15 +0200

    Lint utils class

--- src/Utils.php
+++ src/Utils.php
@@ -13,10 +13,22 @@ defined( 'WPINC' ) || die;
  */
 class Utils {
 
+	/**
+	 * Adds plugin prefix to passed in string. This is used to add our own "namespace" to key values, that we store f.e. to database.
+	 *
+	 * @param string $str - string that needs plugin prefix.
+	 * @return string - string with added plugin prefix.
+	 */
 	public static function add_plugin_prefix( string $str ): string {
 		return Constants::PREFIX . $str;
 	}
 
+	/**
+	 * Helper function that checks if passed in string has plugin prefix.
+	 *
+	 * @param string $str - string that needs checking.
+	 * @return bool - has or has not the prefix.
+	 */
 	public static function has_plugin_prefix( string $str ): bool {
 		return substr( $str, 0, strlen( Constants::PREFIX ) ) === Constants::PREFIX;
 	}
