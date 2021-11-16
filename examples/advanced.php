5e76596e3994724ee409bc6184039ad13e65af82
Atte Liimatainen <atte.liimat@gmail.com>
2021-11-16 21:01:41 +0200

    lint examples and add them to excludes

--- examples/advanced.php
+++ examples/advanced.php
@@ -3,54 +3,54 @@
  * Advanced example for rendering image blur for an image attachment, while main image is loading.
  */
 
-// existing image attachment's id
+// existing image attachment's id.
 $id = 17;
- 
-$prefix = "image_blur_";
 
-// a coined term for the default size
-$image_size = "original";
+$prefix = 'image_blur_';
+
+// a coined term for the default size.
+$image_size = 'original';
 
 $key = $prefix . $image_size;
 
 // this value can be null in various cases, do null checking if necessary.
-$blur_data = get_post_meta($id, $key, true);
+$blur_data = get_post_meta( $id, $key, true );
 
-list($url, $width, $height) = wp_get_attachment_image_src($id, "full");
+list($url, $width, $height) = wp_get_attachment_image_src( $id, 'full' );
 
 ?>
 <div class="image-wrapper">
   <img
-    class="image-blur"
-    role="presentation"
-    src="<?php echo $blur_data; ?>"
+	class="image-blur"
+	role="presentation"
+	src="<?php echo $blur_data; ?>"
   />
   <img
-    class="image"
-    width="<?php echo $width; ?>"
-    height="<?php echo $height; ?>"
-    src="<?php echo $url; ?>"
+	class="image"
+	width="<?php echo $width; ?>"
+	height="<?php echo $height; ?>"
+	src="<?php echo $url; ?>"
   />
 </div>
 
 <style>
   .image-wrapper {
-    position: relative;
-    display: inline-block;
+	position: relative;
+	display: inline-block;
   }
 
   .image-blur {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    transition: 0.4s opacity linear 0.1s;
-    opacity: 1;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	transition: 0.4s opacity linear 0.1s;
+	opacity: 1;
   }
 
   .js-hide {
-    opacity: 0;
+	opacity: 0;
   }
 </style>
 
--- examples/basic.php
+++ examples/basic.php
@@ -3,18 +3,18 @@
  * A basic example for rendering blur for an image attachment.
  */
 
-// Image attachment's id
-$id = 16;
+// Image attachment's id.
+$attachment_id = 16;
 
-$prefix = "image_blur_";
+$prefix = 'image_blur_';
 
-// a coined term for the default size. 
-$image_size = "original";
+// a coined term for the default size.
+$image_size = 'original';
 
 $key = $prefix . $image_size;
 
 // this value can be null in various cases, do null checking if necessary.
-$blur_data = get_post_meta($id, $key, true);
+$blur_data = get_post_meta( $attachment_id, $key, true );
 ?>
 
 <img src='<?php echo $blur_data; ?>' />
