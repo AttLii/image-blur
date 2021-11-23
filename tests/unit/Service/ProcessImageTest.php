333042493810d2ba662d3e4941152bb8fbc3b062
Atte Liimatainen <atte.liimat@gmail.com>
2021-11-23 11:35:33 +0200

    foo

--- tests/unit/Service/ProcessImageTest.php
+++ tests/unit/Service/ProcessImageTest.php
@@ -67,8 +67,7 @@ final class ProcessImageTest extends WP_Mock\Tools\TestCase {
 
 		ob_start();
 		imagejpeg( $mock_image );
-		$mock_image_content = ob_get_contents();
-		ob_end_clean();
+		$mock_image_content = ob_get_clean();
 
 		$this->assertEquals( sha1($mock_image_content, true), sha1_file( "./tests/assets/test-image-gaussian-blur-1.jpg", true ) );
 	}
