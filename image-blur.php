1c9662625059f09dc19d9b21022670b0b7175c99
Atte Liimatainen <atte.liimat@gmail.com>
2022-02-28 22:21:41 +0200

    fix the actual issue

--- image-blur.php
+++ image-blur.php
@@ -12,15 +12,15 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-
-require_once 'vendor/autoload.php';
-
 /**
  * Stop execution if not in Wordpress environment
  */
 defined( 'WPINC' ) || die;
 
-$image_blur_plugin = new ImageBlur\Plugin();
+if ( ! class_exists( 'ImageBlur\Plugin' ) ) {
+	require_once 'vendor/autoload.php';
+	$image_blur_plugin = new ImageBlur\Plugin();
+	register_deactivation_hook( __FILE__, array( $image_blur_plugin, 'deactivate' ) );
+	register_activation_hook( __FILE__, array( $image_blur_plugin, 'activate' ) );
+}
 
-register_deactivation_hook( __FILE__, array( $image_blur_plugin, 'deactivate' ) );
-register_activation_hook( __FILE__, array( $image_blur_plugin, 'activate' ) );
