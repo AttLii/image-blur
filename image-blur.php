cfef70083f978aedc533e19a7b833dd928e115c1
Atte Liimatainen <atte.liimat@gmail.com>
2023-12-10 19:04:01 +0200

    downgrade php versions to 8.2 so deps can be installed

--- image-blur.php
+++ image-blur.php
@@ -6,7 +6,7 @@
  * Version:           2.0.2
  * Requires at least: 6.1.1
  * Tested up to:      6.4.2
- * Requires PHP:      8.3
+ * Requires PHP:      8.2
  * Author:            Atte Liimatainen
  * Author URI:        https://github.com/AttLii
  * License:           GPL v2 or later
