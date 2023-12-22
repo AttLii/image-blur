578930f4d77c59f5e2b877152dd04f24a4851269
Atte Liimatainen <atte.liimat@gmail.com>
2023-12-22 20:23:52 +0200

    update bootstrap and hook tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,6 @@
 <?php
-define('WPINC', true);
 define('SRC_FOLDER', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR);
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+WP_Mock::activateStrictMode();
+WP_Mock::bootstrap();
--- tests/indexTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-describe('index.php', function() {
-    test('defines global variable', function() {
-        include SRC_FOLDER . "/index.php";
-
-        expect(defined('IMAGE_BLUR_PLUGIN_INIT'))->toBeTrue();
-    });
-});
