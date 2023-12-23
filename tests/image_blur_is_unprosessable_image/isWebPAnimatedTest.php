7c67c2db79717882c788c8ddd3505ef66b159735
Atte Liimatainen <atte.liimat@gmail.com>
2023-12-23 14:47:04 +0200

    fix typo

--- tests/image_blur_is_unprosessable_image/isWebPAnimatedTest.php
+++ tests/image_blur_is_unprosessable_image/isWebPAnimatedTest.php
@@ -2,6 +2,6 @@
 
 test('animated webp is unprosessable', function() {
     require SRC_FOLDER . "index.php";
-    $path = dirname(__FILE__) . realpath("/../assets/funImageAnimated.webp");
+    $path = realpath(dirname(__FILE__) . "/../assets/funImageAnimated.webp");
     expect(image_blur_is_unprosessable_image("webp", $path))->toBe(true);
 });
