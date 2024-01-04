99f43a95104347434be322c0d9c2743ba7b30d9e
Atte Liimatainen <atte.liimat@gmail.com>
2024-01-04 21:51:04 +0200

    try chmod trick again

--- tests/image_blur_blur_image/happyPathJpgTest.php
+++ tests/image_blur_blur_image/happyPathJpgTest.php
@@ -1,8 +1,9 @@
 <?php
 
 test('applies expected blur to the image', function () {
-    $gd_image = imagecreatefromjpeg(TEST_ASSET_FOLDER . "/funImage.jpg");
     require SRC_FOLDER . "index.php";
+
+    $gd_image = imagecreatefromjpeg(TEST_ASSET_FOLDER . "/funImage.jpg");
     image_blur_blur_image($gd_image);
 
     ob_start();
