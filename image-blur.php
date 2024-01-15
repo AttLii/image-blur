3b1dbf52caa0a3c38f2e58c1f01bd1d05956e049
Atte Liimatainen <atte.liimat@gmail.com>
2024-01-15 20:21:47 +0200

    add yet another version bump

--- image-blur.php
+++ image-blur.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Image Blur
  * Plugin URI:        https://github.com/AttLii/image-blur
  * Description:       Generates base64 encoded, downscaled and blurred versions of media library's images, which can be used f.e. as a placeholder.
- * Version:           3.0.1
+ * Version:           3.0.2
  * Requires at least: 6.1.1
  * Tested up to:      6.4.2
  * Requires PHP:      8.2
